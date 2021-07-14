7ae00228a110f408f0e6122d8ae077fd3c7d5c4a
Tristan San Juan <tristansj88@gmail.com>
2021-07-14 04:06:11 -0700

    finish note db

--- app/models/note.py
+++ app/models/note.py
@@ -1,7 +1,25 @@
 from .db import db
 from sqlaclhemy.orm import relationship
+from datetime import date, datetime
 
 class Note(db.Model):
     __tablename__ = "notes"
     id= db.Column(db.Integer, primary_key = True)
-    ``
+    content = db.Column(db.Text)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    color = db.Column(db.String)
+    archived = db.Column(db.Boolean, default=False, nullable=False)
+    pinned = db.Column(db.Boolean, default=False, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+    
+
+def to_dict(self):
+    return{
+        "id": self.id,
+        "content": self.content,
+        "user_id": self.user_id,
+        "color": self.color,
+        "archived": self.archived,
+        "pinned": self.pinned,
+        "created_at": self.created_at,
+    }
