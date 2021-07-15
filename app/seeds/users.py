5a0c7532cea1153084108a54159d8c5b8f41ceea
Tristan San Juan <tristansj88@gmail.com>
2021-07-15 02:23:17 -0700

    added more seeders for users + avatar url

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -1,15 +1,28 @@
 # from werkzeug.security import generate_password_hash
+import faker
 from app.models import db, User
+from faker import Faker
 
+faker = Faker()
 
 # Adds a demo user, you can add other users here if you want
 def seed_users():
 
     demo = User(username='Demo', email='demo@aa.io',
-                password='password')
+                password='password', 
+                avatar_url ="https://canary.contestimg.wish.com/api/webimage/5d7dd8619abb1f4feea0d1ab-large.jpg?cache_buster=170b6c55d8be415f8d3e2bd3e8dd38e5")
 
     db.session.add(demo)
-
+    db.session.commit()
+    
+    for i in range(10):
+        other = User(
+            username=f'{faker.first_name()}{faker.random_int(0, 99)}',
+            email=faker.safe_email(),
+            password="password",
+            avatar_url=f'{faker.Avatar.image_url(800,600)}'
+        )
+        db.session.add(other)
     db.session.commit()
 
 
