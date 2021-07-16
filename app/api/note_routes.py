f9bde517a9cfd3c80a84c4af4568e1d8f7dd3d32
Tristan San Juan <tristansj88@gmail.com>
2021-07-15 21:58:34 -0700

    added patch and post routes

--- app/api/note_routes.py
+++ app/api/note_routes.py
@@ -3,13 +3,14 @@ from flask_login import login_required, current_user
 from app.models import Note
 from app.forms import NoteForm
 from sqlalchemy import desc, asc
+from datetime import datetime
 
 note_routes = Blueprint('notes', __name__)
 
 @note_routes.route('/')
 @login_required
 def get_notes():
-    notes = Note.query.all() #check why I cannot use order_by.desc().all()
+    notes = Note.query.order_by(Note.created_at.desc()).all() #check why I cannot use order_by.desc().all()
     return {"notes":{note.id: note.to_dict() for note in notes}}
 
 @note_routes.route('/<int:id>')
@@ -18,3 +19,58 @@ def get_note(id):
     note = Note.query.get(id)
     return note.to_dict()
 
+@note_routes.route('/archive')
+@login_required
+def get_archive_notes():
+    notes = Note.query.filter(Note.archived == True).all()
+    return {"notes":{note.id: note.to_dict() for note in notes}}
+
+@note_routes.route('/pinned')
+@login_required
+def get_archive_notes():
+    notes = Note.query.filter(Note.pinned == True).all()
+    return {"notes":{note.id: note.to_dict() for note in notes}}
+
+
+@note_routes.route('/', methods=["POST"])
+@login_required
+def post_note():
+    new_title = request.json['title']
+    new_content = request.json['content']
+    new_color = request.json["color"]
+    new_archived = request.json["archived"]
+    new_pinned = request.json['pinned']
+    note = Note(
+        title = new_title,
+        content = new_content,
+        user_id = current_user.id,
+        color = new_color,
+        archived = new_archived,
+        pinned = new_pinned,
+        created_at = datetime.utcnow
+        
+    )
+    db.session.add(note)
+    db.session.commit()
+    return {"note": note.to_dict()}
+
+@note_routes.route('/', methods=["PATCH"])
+@login_required
+def post_note():
+    note_id = request.json('noteId')
+    new_title = request.json['title']
+    new_content = request.json['content']
+    new_color = request.json["color"]
+    new_archived = request.json["archived"]
+    new_pinned = request.json['pinned']
+    current_note = Note.query.get(note_id)
+    current_note.title = new_title,
+    current_note.content = new_content,
+    current_note.user_id = current_user.id,
+    current_note.color = new_color,
+    current_note.archived = new_archived,
+    current_note.pinned = new_pinned,
+    current_note.created_at = datetime.utcnow
+    db.session.commit()
+    return {"note": current_note.to_dict()}
+    
