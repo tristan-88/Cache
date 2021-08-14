58c120147ced5b447b0d34ec8697680733e0178d
Tristan San Juan <tristansj88@gmail.com>
2021-08-14 15:16:05 -0700

    added demo button component

--- react-app/src/components/DemoButton/DemoButton.js
+++ react-app/src/components/DemoButton/DemoButton.js
@@ -4,3 +4,23 @@ import { useDispatch } from "react-redux"
 import { demoLogin } from "../../../src/store/session"
 import "./DemoButton.css"
 
+const DemoButton = () => {
+    const history = useHistory()
+    const dispatch = useDispatch()
+
+    const onClick = async (e) => {
+        e.preventDefault()
+        await dispatch(demoLogin())
+        history.push("/main")
+
+    }
+
+    return (
+        <div>
+            <button className="button-demo" onClick={onClick}>Demo</button>
+        </div>
+    )
+
+}
+
+export default DemoButton
