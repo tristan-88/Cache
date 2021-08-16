7b820bdc1734abeded1018fe1d724b19bf355e8f
Tristan San Juan <tristansj88@gmail.com>
2021-08-16 15:20:17 -0700

    deploying

--- react-app/src/store/note.js
+++ react-app/src/store/note.js
@@ -246,7 +246,7 @@ export default function noteReducer(state = initialState, action) {
       );
       //   delete newState.archived[action.payload];
       return newState;
-    
+    //cases for add and remove to pinned and archived slice of state 
     case ADD_PINNED:
       newState = Object.assign({}, state)
       if (action.payload.archived) {
